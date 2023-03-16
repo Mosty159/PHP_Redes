9ef29ccc031b8b2fd447e315f3a6d18dc27e8a8d
Mosty159 <guifilipe41@gmail.com>
2023-03-16 21:39:09 +0000

    oi

---
+++ Ficha4/EXE3.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <title>EXE3</title>
+</head>
+<body>
+    <table border="2" bordercolor="red" align="center" width="10%">
+        <?php
+            if (empty($_POST['num1']) && is_numeric($_POST['num1'])) {
+                 
+            $num1 = $_POST['num1'];
+            $i = 0;
+
+            do{
+                echo "<tr>";
+                echo "<td align = \center\>", $i + 1, "</td>";
+                echo "<td align = \center\>", $num1 + ($i + 1), "</td>";
+                echo "</tr>";
+                $i++;
+            }while($i <= 10)
+        } else{
+            echo "O valor não é um número"
+        }
+        ?>
+    </table>
+    
+    <div align="center">
+        <p><Form action = "EXE3.html" method="POST">   
+            <input type="submit" value = "Continuar" algin="center">
+        </Form></p>
+    </div>
+</body>
+</html>
---
+++ Ficha4/EXE4.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <title>EXE4</title>
+</head>
+<body>
+
+    <Form action = "<?php echo $_SERVER['PHP_SELF'];?>" method="POST">
+
+    <?php
+        if(!isset($_POST)){
+
+        $dia = 1;
+        $meses = array("Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro");
+        $ano = 1980;
+
+        echo 'Dia: <select name = "dia">';
+        for($a = 0; $a < 31; $a++){
+            echo 'option value= "' , $dia ,'">' , $dia , '</option>';
+            $dia++;
+        }
+        echo "</select><br><br>";
+
+        echo 'Mês: <select name = "meses">';
+        for($a = 0; $a < 12; $a++){
+            echo 'option value= "' , $meses[$a] , '">' , $meses[$a] , '</option>';
+        }
+        echo "</select> <br> <br>";
+
+
+        echo 'Ano: <select name = "ano">';
+        for($a = 1980; $a < 2023; $a++){
+            echo 'option value= "' , $ano , '">' , $ano , '</option>';
+            $ano++;
+        }
+        echo "</select> <br> <br>";
+
+        echo '<input type="submit" value = "Mostrar">';
+
+
+        }
+        else{
+
+            $dai = $_POST['$dia'];
+            $mes = $_POST['$meses'];
+            $anu = $_POST['$ano'];
+
+            echo "Foi escolhido o dia $dai do mês de $mes do ano de $anu <br><br>";
+
+            echo '<input type="submit" value="Voltar"';
+        }
+    ?>
+
+    </Form>
+</body>
+</html>
